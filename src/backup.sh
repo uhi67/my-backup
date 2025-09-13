c30b8a44a61026ceb87c2ea9ec90dccf9eca8337
Uherkovich Péter <uherkovich.peter@gmail.com>
2025-09-13 11:52:50 +0200

    fix backup...

--- src/backup.sh
+++ src/backup.sh
@@ -18,7 +18,7 @@ if [ "$APP_DB_NAME" = "" ]; then
 fi
 TIMESTAMP=$(date "+%Y-%m-%d_%H-%M-%S")
 echo "Starting backup into: $APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP.gz"
-mysqldump -h "$APP_DB_HOST" "-u $APP_DB_USER" "-p$APP_DB_PASSWORD" --add-drop-table --single-transaction --routines --triggers "$APP_DB_NAME" 2> "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP".log | gzip -c > "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP.tmp"
+mysqldump -h "$APP_DB_HOST" -u "$APP_DB_USER" -p$APP_DB_PASSWORD --add-drop-table --single-transaction --routines --triggers --no-tablespaces "$APP_DB_NAME" 2> "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP".log | gzip -c > "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP.tmp"
 mv "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP.tmp" "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP.gz"
 cp "$APP_BACKUP_DIR/$APP_DB_NAME-$TIMESTAMP".log "$APP_BACKUP_DIR/last.log"
 echo "Backup finished successfully."
